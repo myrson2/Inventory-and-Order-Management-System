52db75b08c8cae0936829cb9c39cf0f6e7d7e50c
Myrson2 <beros.josemyrson.oreo1@gmail.com>
2026-01-21 23:42:57 +0800

    Add console based user input authentication

--- src/App.java
+++ src/App.java
@@ -1,5 +1,94 @@
+import java.util.Scanner;
+import user.User;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("===== WELCOME TO INVENTORY AND MANAGEMENT SYSTEM =====");
+
+        int userAuthChoice = 0;
+        do{ // start user Authentication 
+            userAuthentication();
+            System.out.print("> ");
+            userAuthChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("====================");
+
+            User user = new User();
+
+            do{ // As long as invalid 
+                switch(userAuthChoice){
+                    case 1:
+                        System.out.println("=== Login ===");
+
+                        System.out.print("ID: ");
+                        String id = scanner.nextLine();
+
+                        System.out.print("Name: ");
+                        String name = scanner.nextLine();
+
+                        System.out.print("Email: ");
+                        String email = scanner.nextLine();
+
+                        if(user.login()){
+                            System.out.println("Successfully login.");
+                        }
+
+                        user = new User(id, name, email);
+
+                    break;
+
+                    case 2:
+                        if(user.logout()){
+                            System.out.println("Logging Out");
+                            scanner.close();
+                        }
+                    break;
+
+                    default:
+                        System.out.println("Invalid Code.");
+                    break;
+                }
+
+            }while(userAuthChoice > 3); // Valid Authentication
+
+            System.out.println("====================");
+
+            //Performing Role Action
+            System.out.println("Hi " + user.getName() + "! Select Your Role: ");
+            
+            userRole();
+            System.out.println("> ");
+            int userRoleChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userRoleChoice) {
+                case 1: // Customer Dashboard
+                    
+                    break;
+            
+                case 2: // Admin Dashboard
+                    
+                    break;
+                    
+                default:
+                    break;
+            }
+        
+        } while(userAuthChoice != 2); // end user authentication
+
+        System.out.println("End System.");
+    }
+
+    static void userAuthentication(){
+        System.out.println("1. login");
+        System.out.println("2. logout");
+    }
+
+    static void userRole(){
+        System.out.println("1. Customer");
+        System.out.println("2. Admin");
     }
 }
---
+++ src/user/User.java
@@ -0,0 +1,40 @@
+package user;
+
+public class User {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(){
+        this("", "", "");
+    }
+
+    public User(String id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public boolean login(){
+        if(getId().isEmpty() || getEmail().isEmpty() || getName().isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean logout(){
+        return true;
+    }
+}
